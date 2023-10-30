3b5dc861c447f839baf40aad11cc8f14b8010a54
Mihai Crisan <crisanmihai2004@gmail.com>
2023-10-30 13:46:04 +0200

    seminar 5

--- seminar5.py
+++ seminar5.py
@@ -2,16 +2,16 @@
 
 def knapsack(w: int, weights: list, values: list) -> int:
 
-    dp = [[0 for _ in range(w + 1)] for _ in range(len(weights) + 1)]
+    dp = [[0 for _ in range(len(weights) + 1)] for _ in range(w + 1)]
 
     for i in range(1, w + 1):
         for j in range(1, len(weights) + 1):
             if weights[j - 1] <= i:
-                dp[j][i] = max(dp[j - 1][i], values[j - 1] + dp[j - 1][i - weights[j - 1]])
+                dp[i][j] = max(dp[i][j - 1], values[j - 1] + dp[i - weights[j - 1]][j - 1])
             else:
-                dp[j][i] = dp[j - 1][i]
+                dp[i][j] = dp[i][j - 1]
     
-    return dp[len(weights)][w]
+    return dp[w][len(weights)]
 
 w = 14
 weights = [1, 2, 3, 5, 9]
