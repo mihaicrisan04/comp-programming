22fd1c255cb01430c4a18e0f5b37c6074655bf2a
Mihai Crisan <crisanmihai2004@gmail.com>
2023-10-30 13:52:13 +0200

    seminar 5

--- seminar5.py
+++ seminar5.py
@@ -1,5 +1,5 @@
 
-
+"""
 def knapsack(w: int, weights: list, values: list) -> int:
 
     dp = [[0 for _ in range(len(weights) + 1)] for _ in range(w + 1)]
@@ -16,5 +16,23 @@ def knapsack(w: int, weights: list, values: list) -> int:
 w = 14
 weights = [1, 2, 3, 5, 9]
 values  = [2, 8, 6, 4, 9]
-
 print(knapsack(w, weights, values))
+"""
+
+
+
+def change_to_sum(n: int, coins: list) -> int:
+    dp = [[0 for _ in range(n + 1)] for _ in range(len(coins) + 1)]
+
+    for i in range(1, len(coins) + 1):
+        for j in range(1, n + 1):
+            if coins[i - 1] <= j:
+                dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]]
+            else:
+                dp[i][j] = dp[i - 1][j]
+
+    return dp[len(coins)][n]
+
+n = 10
+coins = [1, 2, 5, 10]
+print(change_to_sum(n, coins))
