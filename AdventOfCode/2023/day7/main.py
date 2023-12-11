1bd3ffb89275acc1212b906810d34a0e7d096b67
Mihai Crisan <crisanmihai2004@gmail.com>
2023-12-11 22:55:16 +0200

    day 7 started

---
+++ AdventOfCode/2023/day7/main.py
@@ -0,0 +1,16 @@
+
+
+
+def read_file():
+    file_name = 'data.in'
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        return lines
+
+def main() -> None:
+    lines = read_file()
+    
+
+
+if __name__ == '__main__':
+    main()  
