59261af1ba7a7ab6126a57a97606681db23cc65f
Mihai Crisan <crisanmihai2004@gmail.com>
2024-02-15 19:13:43 +0200

    largest rectangle in histogram

--- leetcode/84LargestRectangleInHistogram/main.cpp
+++ leetcode/84LargestRectangleInHistogram/main.cpp
@@ -7,13 +7,32 @@ using namespace std;
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        
+        int max_area = 0;
+        stack<pair<int,int>> st;
+
+        for (int i = 0; i < heights.size(); i++) {
+            int start = i;
+            while (!st.empty() && st.top().second > heights[i]) {
+                max_area = max(max_area, (i - st.top().first) * st.top().second);
+                start = st.top().first;
+                st.pop();
+            }
+            st.push({start, heights[i]});
+        }
+
+        while (!st.empty()) {
+            max_area = max(max_area, (int)((heights.size() - st.top().first) * st.top().second));
+            st.pop();
+        }
+
+        return max_area;
     }
 };
 
 
 int main() {
     Solution s;
-
+    vector<int> heights = {2,1,5,6,2,3};
+    cout << s.largestRectangleArea(heights) << endl;
     return 0;
 }
