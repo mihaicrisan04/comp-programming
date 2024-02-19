d243ba0cd907d1185eabcdf9dbf9fd53cfb5d9ee
Mihai Crisan <crisanmihai2004@gmail.com>
2024-02-19 21:04:58 +0200

    work in progress:

---
+++ leetcode/4MedianOfTwoSortedArrays/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // doua cautari binare in paralel
+        int l = 0, ll = 0, r = nums1.size() - 1, rr = nums2.size() - 1;
+        while (l <= r && ll <= rr) {
+            break;
+        }
+    }
+};
+
+
+int main() {
+    Solution s;
+
+    return 0;
+}
