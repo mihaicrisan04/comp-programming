8147df1847b1c937ccc5b4f5bacaeb17b265c481
Mihai Crisan <crisanmihai2004@gmail.com>
2024-02-29 22:41:36 +0200

    reverse linked list

---
+++ leetcode/206ReverseLinkedList/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    struct ListNode* next = NULL;
+
+    while (curr != NULL) { 
+        next = curr->next; 
+        curr->next = prev; 
+        prev = curr; 
+        curr = next;
+    } 
+
+    return prev;
+}
+
+
+
+int main() {
+    struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->val = 1;
+    head->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->next->val = 2;
+    head->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->next->next->val = 3;
+    head->next->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->next->next->next->val = 4;
+    head->next->next->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->next->next->next->next->val = 5;
+    head->next->next->next->next->next = NULL;
+
+    struct ListNode* reversed = reverseList(head);
+    while (reversed != NULL) {
+        printf("%d\n", reversed->val);
+        reversed = reversed->next;
+    }
+    return 0;
+}
