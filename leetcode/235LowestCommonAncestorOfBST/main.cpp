27050357974180b458d867ef2e34dfc38fe7d074
Mihai Crisan <crisanmihai2004@gmail.com>
2024-06-13 22:48:57 +0300

    a

---
+++ leetcode/235LowestCommonAncestorOfBST/main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root->val == p->val || root->val == q->val) return root;
+        if (root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
+        else if (root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+        else return root;
+    }
+};
+
+
+int main() {
+    Solution s;
+
+    return 0;
+}
