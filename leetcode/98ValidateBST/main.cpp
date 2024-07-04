da49292c7a064d6b6cb07c966d9b5b34df575ada
Mihai Crisan <crisanmihai2004@gmail.com>
2024-07-04 23:14:34 +0300

    d

---
+++ leetcode/98ValidateBST/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool isValidBST(TreeNode* root, long min, long max) {
+        if (!root) return true;
+        if (root->val <= min || root->val >= max) return false;
+        return isValidBST(root->left, min, root->val) && isValidBST(root->right, root->val, max);
+    }
+};
+
+
+int main() {
+    Solution s;
+
+    return 0;
+}
