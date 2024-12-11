2171035d8e332d4fd74d1371db0628edd301a30a
Mihai Crisan <crisanmihai2004@gmail.com>
2024-12-11 16:40:13 +0200

    x

---
+++ leetcode/300LongestIncreasingSubsequence/main.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        dp = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
+
+s = Solution()
+print(s.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+print(s.lengthOfLIS([0, 1, 0, 3, 2, 3]))
+print(s.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]))
+
+
+       
