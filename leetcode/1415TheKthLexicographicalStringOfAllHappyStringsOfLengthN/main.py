a42c64231a6849ec1c2d9a09a6074122ce64dc68
Mihai Crisan <crisanmihai2004@gmail.com>
2025-02-19 17:44:59 +0200

    x

---
+++ leetcode/1415TheKthLexicographicalStringOfAllHappyStringsOfLengthN/main.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        options = 'abc'
+        sol = ''
+
+    def back(n, k, sol, options):
+
+
+s = Solution()
+print(s.getHappyString(1, 3))
+print(s.getHappyString(1, 4))
+print(s.getHappyString(3, 9))
